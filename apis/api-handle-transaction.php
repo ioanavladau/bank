2df1f073816e7faef25ee16e63b6d27e2ae658ef
Ioana Vladau <ioan0414@stud.kea.dk>
2019-02-14 12:39:06 +0100

    I AM GETTING MSGS FROM BANKS AND OTHER PPL ARE GETTING MY MSG

--- apis/api-handle-transaction.php
+++ apis/api-handle-transaction.php
@@ -1,46 +1,31 @@
 <?php
-// header('Access-Control-Allow-Origin: *');
-// validate we have the key
 
+ini_set('user_agent', 'any');
 ini_set('display_errors', 0);
 
 $sData = file_get_contents('../data/clients.json');
-// convert it into an object
 $jData = json_decode( $sData );
-if( $jData == null ){ fnvSendResponse(-1, __LINE__, 'Cannot convert the data file to JSON'); }
+if( $jData == null ){ fnvSendResponse(-1, __LINE__, 'Cannot convert the data file to json'); }
 $jInnerData = $jData->data;
 
 $sPhoneFromOtherServer = $_GET['phone'];
-$iAmountToTransfer = $_GET['amount'];
-// fetch amount
+// $iAmountToTransfer = $_GET['amount'];
 
-if( ! $jInnerData->$sPhoneFromOtherServer ){
+if( ! $jInnerData->$sPhoneFromOtherServer ){   
   fnvSendResponse(0, __LINE__, 'Phone not registered in BANK VLADAU');
 }
 
-$jInnerData->$sPhoneFromOtherServer->balance += $iAmountToTransfer;
-
+// echo $jInnerData->$sPhoneFromOtherServer->balance;
+// exit;
+// $jInnerData->$sPhoneFromOtherServer->balance += $iAmountToTransfer;
 $sData = json_encode($jData);
-if( $sData == null ) { sendResponse(0, __LINE__, 'Could not write to file'); }
-file_put_contents('../data/clients.json', $sData); // unknown
-
-
-// before I find the match talk to json object and update the balance to be the balance + 
+// if( $sData == null ) { sendResponse(-1, __LINE__); }
+// file_put_contents('../data/clients.json', $sData); 
 fnvSendResponse(1, __LINE__, 'Transaction success from BANK VLADAU');
 
+
 // **************************************************
-
-function fnvSendResponse( $iStatus, $iLine, $sMessage ){
-  echo '{"status": '.$iStatus.', "code": '.$iLine.', "message": "'.$sMessage.'"}';
-  exit; // exit kills the PHP script
+function fnvSendResponse( $iStatus, $iLineNumber, $sMessage ){
+  echo '{"status":'.$iStatus.', "code":'.$iLineNumber.', "message":"'.$sMessage.'"}';
+  exit;
 }
-
-// echo 'VISITING BANK VLADAU';
-// if the phone exists in this bank
-// get the amount and the message
-// and set the new balance to the phone number
-// then reply to the server saying that
-// the transaction was successful
-// else
-// no prone or sth went wrong, reply with error
-
--- apis/api-transfer.php
+++ apis/api-transfer.php
@@ -1,87 +1,76 @@
 <?php
 
-ini_set('display_errors', 0);
 ini_set('user_agent', 'any');
+ini_set('display_errors', 0);
 
-// AJAX!
-// constrain it for logged users
 session_start();
-if( !isset($_SESSION['sUserId']) ) { // or !isset($_SESSION['sUserId']) 
+if( !isset($_SESSION['sUserId'] ) ){
   sendResponse(-1, __LINE__, 'You must login to use this api');
 }
 
-// $sPhoneToCheck = $_GET['phone'] || '';
-if(empty($_GET['phone'])){ sendResponse(-1, __LINE__, 'Phone is missing'); }
-if(empty($_GET['amount'])){ sendResponse(-1, __LINE__, 'Amount is missing'); }
+if( empty( $_GET['phone'] ) ){ sendResponse(-1, __LINE__, 'Phone missing'); }
+if( empty( $_GET['amount'] ) ){ sendResponse(-1, __LINE__, 'Amount is missing'); }
+
+
 
 $sPhone = $_GET['phone'] ?? '';
-if( strlen($sPhone) != 8 ) { sendResponse(-1, __LINE__, 'Phone must be 8 characters in length'); }
-if( ctype_digit($sPhone) == false ) { sendResponse(-1, __LINE__, 'Phone can only be numbers'); }
+if( strlen($sPhone) != 8 ){ sendResponse(-1, __LINE__, 'Phone must be 8 characters in length'); }
+if( !ctype_digit($sPhone)  ){ sendResponse(-1, __LINE__, 'Phone can only contain numbers');  }
 
-$sAmount = $_GET['amount'] ?? '';
-if( strlen($sPhone) != 8 ) { sendResponse(-1, __LINE__, 'Phone must be 8 characters in length'); }
-if( ctype_digit($sPhone) == false ) { sendResponse(-1, __LINE__, 'Phone can only be numbers'); }
-
-// validate amount
+// Validate amount
 $iAmount = $_GET['amount'] ?? '';
 
-
-
-
 $sData = file_get_contents('../data/clients.json');
-// echo $sData;
-$jData = json_decode($sData);
-if( $jData == null ) { sendResponse(-1, __LINE__, 'Cannot convert data to JSON'); }
+$jData = json_decode( $sData );
+
+if( $jData == null){ sendResponse(-1, __LINE__, 'Cannot convert data to JSON');  }
 
 $jInnerData = $jData->data;
 
-if( ! $jInnerData->$sPhone ){
+if( !$jInnerData->$sPhone ){ 
   $jListOfBanks = fnjGetListOfBanksFromCentralBank();
   // loop through the list
   // connect to each bank
-
   foreach( $jListOfBanks as $sKey => $jBank ){
     // echo $jBank->url;
     // echo $jBank->key;
-    $sUrl = $jBank->url.'/apis/api-handle-transaction?phone='.$sPhone; // expand code with amount
-    // echo "<div>$sUrl</div>";
-    $sBankResponse = file_get_contents($sUrl);
+    // $sUrl = $jBank->url.'/apis/api-handle-transaction?phone='.$sPhone.'&amount='.$iAmount;
+    $sUrl = $jBank->url.'/apis/api-handle-transaction?phone='.$sPhone.'&amount='.$iAmount;
+    // echo $sUrl.'<br>';
+    $sBankResponse =  file_get_contents($sUrl);
     $jBankResponse = json_decode($sBankResponse);
+
     if( $jBankResponse->status == 1 && 
         $jBankResponse->code && 
-        $jBankResponse->message ){
-          sendResponse(1, __LINE__, $jBankResponse->message );
-          // if sendResponse did not have exit, we should have exited
+        $jBankResponse->message ){ 
+          sendResponse( 1, __LINE__ , $jBankResponse->message );
     }
+
   }
-  
-  sendResponse(2, __LINE__, 'Phone does not exist' );
+  sendResponse( 2, __LINE__ , 'Phone does not exist' );
 }
 
-sendResponse(1, __LINE__, 'Phone registered locally');
+
+sendResponse( 1, __LINE__ , 'Phone registered locally'  );
+// Continue transfering the money
+// Take money from the logged user
+// Give it to the corresponding phone  
+
 
 
 // getListOfBanksFromCentralBank();
 
-
-
-// sendResponse(1, __LINE__, 'Phone is valid');
-// Continue transfering the money 
-// Take money from the logged user 
-// Give it to the corresponding phone
-
-
-
 // **************************************************
-
 function sendResponse($iStatus, $iLineNumber, $sMessage){
-  echo '{"status": '.$iStatus.', "code": '.$iLineNumber.', "message": "'.$sMessage.'"}';
+  echo '{"status":'.$iStatus.', "code":'.$iLineNumber.',"message":"'.$sMessage.'"}';
   exit;
 }
 
-
+// **************************************************
 function fnjGetListOfBanksFromCentralBank(){
   // get the list of banks
   $sData = file_get_contents('https://ecuaguia.com/central-bank/api-get-list-of-banks.php?key=1111-2222-3333');
   return json_decode($sData);
 }
+
+
